134d2a1b2bd7478fe68a13ac9795e69d5c4b9c93
Bunyamin Demiryurek <119034644+bunyamindemiryurek@users.noreply.github.com>
2023-02-18 13:26:18 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:15,
+            nav:true,
+            loop:false
+        }
+    }
+});
+
+$('.owl-carousel-1').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:15,
+            nav:true,
+            loop:false
+        }
+    }
+});
